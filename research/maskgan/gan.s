b459bb2aaaa15bc58731122e67e2296af3623dc1
luciay <lyu571@nyu.edu>
2018-04-30 15:25:18 -0400

    added step_size and max_step to save by global ckpt

--- research/maskgan/gan.s
+++ research/maskgan/gan.s
@@ -27,6 +27,8 @@ python $base_dir/train_mask_gan.py \
  --base_directory=$base_dir \
  --mask_strategy=contiguous \
  --maskgan_ckpt=None \
+ --step_size=1000 \
+ --max_step=200000 \
  --hparams="gen_rnn_size=650,dis_rnn_size=650,gen_num_layers=2,dis_num_layers=2,gen_learning_rate=0.000038877,gen_learning_rate_decay=1.0,gen_full_learning_rate_steps=2000000,gen_vd_keep_prob=0.33971,rl_discount_rate=0.89072,dis_learning_rate=5e-4,baseline_decay=0.99,dis_train_iterations=2,dis_pretrain_learning_rate=0.005,critic_learning_rate=5.1761e-7,dis_vd_keep_prob=0.71940" \
  --mode='TRAIN' \
  --max_steps=1 \
